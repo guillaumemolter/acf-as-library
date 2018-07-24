2cfd4f773a4e9d2c9e3c878d1ded8cb62c570a52
guillaumemolter <gmolter@hsph.harvard.edu>
2018-07-23 22:27:14 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # acf-as-library
-Easy and clean config of advanced custom fields as composer library
+Easy and clean config of the Advanced Custom Fields WordPress plugin to load it as a composer library.
+
+Simplify the process described here: https://www.advancedcustomfields.com/resources/including-acf-in-a-plugin-theme/
+
+## How to use:
+1. Add ACF to your plugin or theme using composer using [PhilippBaschke/acf-pro-installer](https://github.com/PhilippBaschke/acf-pro-installer)
+2. Then add this library to your plugin or theme `composer require guillaumemolter/acf-as-library`
+3. Finally inside of your plugin or theme:
+
+```
+<?php
+include 
+require_once("vendor/guillaumemolter/acf-pro-installer/src/ACFAsLibrary/ACF_Lib.php"); // Not necessary if you use composer autoload.
+
+$path_to_acf =  $path_to_plugin . '/vendor/advanced-custom-fields/advanced-custom-fields-pro/'; // Replace $path_to_plugin by the actual path.
+$url_to_acf =  $url_to_plugin . '/vendor/advanced-custom-fields/advanced-custom-fields-pro/'; // Replace $url_to_plugin by the actual url.
+
+$acf_as_library = new ACF_Lib( $path_to_acf, $url_to_acf );
+$acf_as_library->init();
+?>
---
+++ src/ACFAsLibrary/ACF_Lib.php
@@ -0,0 +1,74 @@
+<?php
+namespace GuillaumeMolter\ACFAsLibrary;
+
+/**
+ * ACF WordPress Plugin as a library.
+ */
+class ACF_Lib{
+
+    /**
+     * The path to the ACF files.
+     *
+     * @var string
+     */
+    public $path_to_acf;
+
+    /**
+     * The url to the ACF files.
+     *
+     * @var string
+     */
+    public $url_to_acf;
+
+    /**
+     * Hide/show the ACF wp admin menu.
+     *
+     * @var boolean
+     */
+    public $show_admin;
+
+    /**
+     * Class constructor.
+     *
+     * @param string $path_to_acf The path to the ACF files
+     * @param string $url_to_acf The url to the ACF files
+     * @param boolean $show_admin Hide/show the ACF wp admin menu.
+     * @return void
+     */
+    function __constructor( $path_to_acf, $url_to_acf,  $show_admin = false ) {
+        $this->path_to_acf = $path_to_acf;
+        $this->url_to_acf = $path_to_acf;
+
+        if( $show_admin === true ) {
+            $this->show_admin = true;
+        } else {
+            $this->show_admin = false;
+        }
+    }
+
+    /**
+     * Register the required ACF WordPress hooks.
+     *
+     * @return void
+     */
+    public function init() {
+        try {
+            // Change ACF path.
+            add_filter( 'acf/settings/path', function () {
+                $this->path_to_acf;
+            } );
+            // Change ACF url.
+            add_filter( 'acf/settings/dir',  function () {
+                $this->url_to_acf;
+            } );
+            // Hide the ACF menu item.
+            if( $show_admin === false ) {
+                add_filter( 'acf/settings/show_admin', '__return_false' );
+            }
+            // Include the ACF loader.
+            include_once $this->path_to_acf . 'acf.php';
+        } catch ( Exception $e ) {
+            echo 'ACFAsLibrary caught an exception: ' .  $e->getMessage() . "\n";
+        }
+    }
+}
